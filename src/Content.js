93daea098735f151b1d94ece45bfac49f19fa194
kirubakaran <kirubak492@gmail.com>
2024-05-08 12:55:23 +0000

    upskill concepts

--- src/Content.js
+++ src/Content.js
@@ -1,22 +1,43 @@
 import React from 'react'
+import { useState } from 'react';
+
 
 const Content = () => {
-  function namechange(){
-    const name=["Earn", "Grow","Give"];
-    const int =Math.floor(Math.random()*3);
-    return name[int]
-  }
-
-  const handleclick=(e)=>{
-    console.log(e.target.innerText)
-  }
-  const handleclick2=(name)=>{
-    console.log("Thank you for support",name)
-  }
+  
+  const [items,setItem]=useState([
+    {
+      id:1,
+      checked:true,
+      item: "practice coding"
+    },
+    {
+      id:2,
+      checked:true,
+      item: "Learn React and Django"
+    },
+    {
+      id:3,
+      checked:true,
+      item: "Learn AI"
+    }
+  ])
+  
+    
   return (
     <main>
-      <p onDoubleClick={()=>handleclick2('kiruba')}>Lets {namechange()} money</p>
-      <button onClick={(e)=>handleclick(e)}>Subscribe</button>
+      <ul>
+        {items.map((item)=>(
+          <li>
+            <input type="checkbox" 
+             checked={item.checked}
+             />
+             <label>{item.item}</label>
+             <button>delete</button>
+          </li>
+         
+        ))}
+      
+      </ul>
     </main>
     
   )
